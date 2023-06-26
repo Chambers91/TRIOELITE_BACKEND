cf1695c577efb785060f657a421e455f8c45888b
Terence Chambers <terencechambers91@gmail.com>
2023-06-25 21:29:03 -0400

    declaring properties

---
+++ src/main/java/com/trioelite/Backend/User.java
@@ -0,0 +1,14 @@
+package com.trioelite.Backend;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDate dob;
+    private Integer age;
+}
